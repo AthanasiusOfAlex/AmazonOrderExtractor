b0568727a13ce33ad0209f4f36455d086441fd72
AthanasiusOfAlex <lmelahn@legionaries.org>
2018-12-06 14:38:05 +0100

    Made tests

--- Sources/AmazonOrderExtractor/AmazonOrderExtractor.swift
+++ Sources/AmazonOrderExtractor/AmazonOrderExtractor.swift
@@ -1,3 +1,18 @@
-struct AmazonOrderExtractor {
-    var text = "Hello, World!"
+import ScriptingUtilities
+import MicrosoftOutlookScripting
+
+enum AmazonOrderConstants: String {
+    case receiptCategory = "Receipt to print"
+}
+
+func extractTextFromMessages() {
+    let outlook = application(name: "Microsoft Outlook") as! MicrosoftOutlookApplication
+    
+    outlook.activate()
+    
+    for message in outlook.inbox!.messages!() {
+        let message = message as! MicrosoftOutlookMessage
+        
+        print(message.content ?? "")
+    }
 }
