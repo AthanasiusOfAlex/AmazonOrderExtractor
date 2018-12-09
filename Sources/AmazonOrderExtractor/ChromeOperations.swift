8a3b258601897c221caed7958d06f0bd8f60add3
AthanasiusOfAlex <lmelahn@legionaries.org>
2018-12-09 13:24:15 +0100

    Now we can open links up in Chrome

--- Sources/AmazonOrderExtractor/ChromeOperations.swift
+++ Sources/AmazonOrderExtractor/ChromeOperations.swift
@@ -6,23 +6,49 @@
 //
 
 import Foundation
-//import GoogleChromeScripting
-//import ScriptingUtilities
-import ScriptingBridge
 
 public func openLinksInChrome() {
-    let script = NSAppleScript(source: """
-        set myLink to "https://www.google.com/"
-        tell application "Google Chrome"
+    do {
+        func runAppleScript(script: String) {
+            let script = NSAppleScript(source: script)!
+            var dict = NSDictionary()
+            withUnsafeMutablePointer(to: &dict) {
+                let errorInfo = AutoreleasingUnsafeMutablePointer<NSDictionary?>($0)
+                script.executeAndReturnError(errorInfo)
+            }
+        }
+        
+        func fillFirstTab(withUrl url: String) {
+            let script = """
+            set myLink to "\(url)"
+            tell application "Google Chrome"
             set myWindow to (make new window)
+            set myTab to active tab of myWindow
+            set URL of myTab to myLink
+            end tell
+            """
+            runAppleScript(script: script)
+        }
+        
+        func fillOtherTab(withUrl url: String) {
+            let script = """
+            set myLink to "\(url)"
+            tell application "Google Chrome"
             tell front window to make new tab at after (get active tab) with properties {URL:myLink}
-        end tell
-    """)!
-    
-    var dict = NSDictionary()
-    withUnsafeMutablePointer(to: &dict) {
-        let errorInfo = AutoreleasingUnsafeMutablePointer<NSDictionary?>($0)
-        //script.compileAndReturnError(errorInfo)
-        script.executeAndReturnError(errorInfo)
+            end tell
+            """
+            runAppleScript(script: script)
+        }
+        
+        let links = getReceiptMessages().map{ $0.getOrderSummaryLink() }.filter { $0 != nil }.map { $0! }
+        
+        if let firstLink = links.first {
+            print(firstLink)
+            fillFirstTab(withUrl: firstLink)
+        }
+        
+        for link in links.dropFirst() {
+            fillOtherTab(withUrl: link)
+        }
     }
 }
