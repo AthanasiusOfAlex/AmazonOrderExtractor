a252c390e35cc359b7c91c43c2a9b0b4b666705a
AthanasiusOfAlex <lmelahn@legionaries.org>
2018-12-11 16:00:30 +0100

    remove dependency to PathKit

--- Package.swift
+++ Package.swift
@@ -15,14 +15,13 @@ let package = Package(
         .package(url: "https://github.com/AthanasiusOfAlex/MicrosoftOutlookScripting.git", .branch("master")),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", .branch("master")),
         .package(url: "https://github.com/sharplet/Regex.git", .branch("master")),
-        .package(url: "https://github.com/kylef/PathKit.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AmazonOrderExtractor",
-            dependencies: ["MicrosoftOutlookScripting","SwiftSoup", "Regex", "PathKit"]),
+            dependencies: ["MicrosoftOutlookScripting","SwiftSoup", "Regex"]),
         .testTarget(
             name: "AmazonOrderExtractorTests",
             dependencies: ["AmazonOrderExtractor"]),
